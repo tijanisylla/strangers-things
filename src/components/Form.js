04ec5f4f310be7fb54b9b0e1f835d1ed8b7f6f63
tijani sylla <tijanisylla@tijanis-Air.attlocal.net>
2021-05-28 14:34:47 -0500

    john's commit

---
+++ src/components/Form.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+ const Form = () => {
+    return (
+        <div>
+
+          
+            <form>
+           
+           <label for="username">Username:</label>
+
+                <input type="text"  placeholder="user-name" required/>
+
+                <label for="password">password:</label>
+
+                <input type="password" placeholder="password" required/>
+
+
+                <label for="password">confirm-password:</label>
+                <input type="password" placeholder="confirm-password" required/>
+
+                
+                <input type="submit"/>
+
+           
+
+
+            </form>
+        </div>
+    )
+}
+
+
+export default Form 
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Form} from './components'
+
+
+ const App = ()=> {
+  
+  return  (
+    <div>
+  <Form /> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
